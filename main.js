3a39f511c14207197b3bbd7160d3586d36fbd133
Anna sganderla <sganderlakarlaanna@gmail.com>
2021-06-01 14:54:17 -0300

    add js

---
+++ main.js
@@ -0,0 +1,36 @@
+var list = [
+    {
+        "desc": "trufa1",
+        "amount": 1,
+        "value": 1.11
+    },
+    {
+        "desc": "trufa2",
+        "amount": 1,
+        "value": 1.11
+    },
+    {
+        "desc": "trufa3",
+        "amount": 1,
+        "value": 1.11
+    }
+]
+
+var total = 10
+
+if (total >= 10) {
+    document.querySelector('.free')
+    .classList.add('active')
+} else {
+    document.querySelector('.free')
+    .classList.remove('active')
+}
+
+
+
+
+
+// pegar valores
+// somar todos eles
+// chegar ao total 
+// se total maior que 10 exibir frete gratis
